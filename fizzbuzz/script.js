d34af5839f21def5a5022e184cab6e75b4236b9a
Gino Villalpando <gvillalpando@cnm.edu>
2020-03-09 02:18:25 -0600

    added better names for variables for readability

--- fizzbuzz/script.js
+++ fizzbuzz/script.js
@@ -1,16 +1,19 @@
-let fizzBuzzGame = (s1 = 'fizz', s2 = 'buzz', n1 = 3, n2 = 5) => {
+//create a variable that passes a function that will be our fizzbuzz game with variables that can be changed
+let fizzBuzzGame = (string1 = 'fizz', string2 = 'buzz', num1 = 3, num2 = 5) => {
 
+    //create variable that will be an array
     let output = [];
 
-
+    //use for loop to iterate array starting at the index of 0 and incrementing each time the loop has finished
     for(let i = [0]; output.length <= 100; i++){
-
-        if( i % n1 === 0 && i % n2 === 0 ) {
-             output.push(s1 + s2); 
-        } else if( i % n1 === 0 ) { 
-            output.push(s1);
-        } else if ( i % n2 === 0 ) { 
-            output.push(s2);
+        
+        //if, else if, and else statements that check if the number variables are divisble
+        if( i % num1 === 0 && i % num2 === 0 ) {
+             output.push(string1 + string2); 
+        } else if( i % num1 === 0 ) { 
+            output.push(string1);
+        } else if ( i % num2 === 0 ) { 
+            output.push(string2);
         } else {
             output.push(i);
         }
@@ -18,4 +21,4 @@ let fizzBuzzGame = (s1 = 'fizz', s2 = 'buzz', n1 = 3, n2 = 5) => {
     return output;
 }
 
-console.log(fizzBuzzGame())
+console.log(fizzBuzzGame('Im', 'Tired', 2, 10))
