fb926df0dd1677c9d9c215becf1975bc4adab4c2
Gino Villalpando <gvillalpando@cnm.edu>
2020-03-09 02:10:34 -0600

    finished for loop that iterates array and increments until equal or less than 100

--- fizzbuzz/script.js
+++ fizzbuzz/script.js
@@ -1,9 +1,9 @@
-let fizzBuzzGame = function(s1 = 'fizz', s2 = 'buzz', n1 = 3, n2 = 5) {
+let fizzBuzzGame = (s1 = 'fizz', s2 = 'buzz', n1 = 3, n2 = 5) => {
 
     let output = [];
 
 
-    for(let i = 1; output.length <= 100; i++){
+    for(let i = [0]; output.length <= 100; i++){
 
         if( i % n1 === 0 && i % n2 === 0 ) {
              output.push(s1 + s2); 
