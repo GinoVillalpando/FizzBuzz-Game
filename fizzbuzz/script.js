b55d31040e5976a30972e54bbc0dc39587113f67
Gino Villalpando <grievxus@outlook.com>
2020-03-11 15:56:08 -0600

    added DOM manipulators but not recieving what's being typed in input 1 for string1

--- fizzbuzz/script.js
+++ fizzbuzz/script.js
@@ -1,7 +1,14 @@
-//create a variable that passes a function that will be our fizzbuzz game with variables that can be changed
-let fizzBuzzGame = (string1 = 'fizz', string2 = 'buzz', num1 = 3, num2 = 5) => {
+let string1 = document.querySelector("input[name='firstInput']").innerHTML
+// let string2 = document.querySelector("#secondInput").innerHTML
+// let num1 = document.querySelector("#firstDivisble").innerHTML
+// let num2 = document.querySelector("#secondDivisible").innerHTML
 
-    //create variable that will be an array
+
+
+//create a variable that passes a function that will be our fizzbuzz game with variables that can be changed
+let fizzBuzzGame = (string1, string2 = 'buzz', num1 = 3, num2 = 5) => {
+
+    //create variable that will be an arrayhttps://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/change_event
     let output = [];
 
     //use for loop to iterate array starting at the index of 0 and incrementing each time the loop has finished
@@ -9,7 +16,7 @@ let fizzBuzzGame = (string1 = 'fizz', string2 = 'buzz', num1 = 3, num2 = 5) => {
         
         //if, else if, and else statements that check if the number variables are divisble
         if( i % num1 === 0 && i % num2 === 0 ) {
-             output.push(string1 + string2); 
+             output.push(string1 + " " + string2); 
         } else if( i % num1 === 0 ) { 
             output.push(string1);
         } else if ( i % num2 === 0 ) { 
@@ -21,4 +28,6 @@ let fizzBuzzGame = (string1 = 'fizz', string2 = 'buzz', num1 = 3, num2 = 5) => {
     return output;
 }
 
-console.log(fizzBuzzGame('Im', 'Tired', 2, 10))
+console.log(string1);
+
+console.log(fizzBuzzGame(string1, 'Tired', 2, 10));
