4853285db52d1902fd697252193ab535e4fff8d7
Gino Villalpando <gvillalpando@cnm.edu>
2020-03-16 13:59:14 -0600

    cleaned up script and added function onchange for p tag

--- fizzbuzz/script.js
+++ fizzbuzz/script.js
@@ -4,25 +4,21 @@
  * 
  * author: Gino Villalpando GinoVillalpandoWork@gmail.com
  */
-document.addEventListener("change", (event) => {
-
-let string1 = document.querySelector("input[name='firstInput']").value;
-let string2 = document.querySelector("input[name='secondInput']").value;
-let num1 = document.querySelector("input[name='firstNum']").value;
-let num2 = document.querySelector("input[name='secondNum']").value;
-
-//run these blocks of code when the word has been entered for the first input
-
+document.addEventListener("change", () => {
 
     //FizzBuzz Function that will manipulate our variables set from the DOM
-    let fizzBuzzGame = (string1, string2, num1, num2) => {
+    function fizzBuzzGame() {
 
-
+        let string1 = document.querySelector("input[name='firstInput']").value;
+        let string2 = document.querySelector("input[name='secondInput']").value;
+        let num1 = document.querySelector("input[name='firstNum']").value;
+        let num2 = document.querySelector("input[name='secondNum']").value;
+        
         //create variable that will be an array
         let output = [];
 
         //use for loop to iterate array starting at the index of 0 and incrementing each time the loop has finished
-        for(let i = [0]; output.length <= 100; i++){
+        for(let i = 1; i <= 100; i++){
             
             //if, else if, and else statements that check if the number variables are divisble
             if( i % num1 === 0 && i % num2 === 0 ) {
@@ -34,10 +30,7 @@ let num2 = document.querySelector("input[name='secondNum']").value;
             } else {
                 output.push(i);
             }
-
-        }
-        return output;
-    }
-    console.log(fizzBuzzGame(string1, string2, num1, num2));
+        } return output;
+    } console.log(fizzBuzzGame());
 });
 
